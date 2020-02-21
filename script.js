61b4e2d3dc1f27488afe812d5efaaf632159bce6
bywdsg <liuyao928@tom.com>
2020-02-21 22:22:38 +1000

    enable validate

--- script.js
+++ script.js
@@ -9,11 +9,10 @@ const inputValue = document.getElementsByTagName('input');
 const stTypeValue = document.getElementById('street-type');
 const resultItems = document.getElementsByClassName('result__item');
 const stTypeResult = document.querySelector('.result__item--select');
-console.log(inputValue);
-prevBtn.addEventListener('click', () => nextPrev(-1));
-nextBtn.addEventListener('click', () => nextPrev(1));
+
 showForm(currentForm);
 function showForm(num) {
+  //show current form
   forms[num].style.display = 'block';
   // show button
   if (!num) {
@@ -33,14 +32,19 @@ function showForm(num) {
 }
 
 function nextPrev(num) {
-  // if (num === 1 && !validateForm())
-  //   return false; 
+  // 当前页有 invalidate
+  if (num === 1 && !isFormValid())
+    return false;
+  //不显示当前页，更新页码，显示下一页
   forms[currentForm].style.display = "none";
-  currentForm = currentForm + num;
-  if (currentForm >= forms.length) {
+  currentForm += num;
+  // 当前页为最后一页时
+  if (currentForm === forms.length) {
+    //删除表格，显示结果
     form.style.display = "none";
     result.style.display = 'block';
-    for (i = 0; i < inputValue.length; i++) {
+    //赋予结果数据
+    for (let i = 0; i < inputValue.length; i++) {
       const info = inputValue[i].value;
       resultItems[i].innerHTML = info;
     }
@@ -49,6 +53,67 @@ function nextPrev(num) {
   }
   showForm(currentForm);
 }
+function isFormValid() {
+  let valid = true;
+  const shownForm = forms[currentForm];
+  console.log(shownForm);
+  const shownInputs = shownForm.getElementsByTagName('input');
+  const shownSpans = shownForm.getElementsByClassName('notice');
+  for (let i = 0; i < shownInputs.length; i++) {
+    if (!isFormatValid(shownInputs[i])) {
+      shownSpans[i].style.display = 'inline';
+      valid = false;
+    }
+  }
+  if (currentForm) {
+    const shownSelect = shownForm.getElementById('street-type');
+    const shownSelectSpan = shownForm.querySelector('.notice--select');
+    if (shownSelect.value === '') {
+      shownSelectSpan.style.display = 'inline';
+      valid = false;
+    }
+  }
+  //只要有一个 valid=false 就 return false
+  return valid;
+}
+function isFormatValid(input) {
+  switch (input.name) {
+    case 'email':
+      return isEmailValid(input.value);
+    case 'phone':
+      return isPhoneValid(input.value);
+    case 'postcode':
+      return isPostcodeValid(input.value);
+    case 'street-number':
+      return isStreetNumValid(input.value);
+    case 'first-name' || 'last-name' || 'street-name' || 'suburb':
+      return isTextValid(input.value);
+    default:
+      return true;
+  }
+};
+function isEmailValid(value) {
+  const emailFormat = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  return emailFormat.test(value);
+}
+function isPhoneValid(value) {
+  const phoneFormat = /^(\+?\(61\)|\(\+?61\)|\+?61|\(0[23478]\)|0[23478])?( ?-?[0-9]){7,9}$/;
+  return value === '' || phoneFormat.test(value);
+}
+function isPostcodeValid(value) {
+  const postcode = parseInt(value, 10);
+  return (postcode >= 800 && postcode <= 7999);
+}
+function isStreetNumValid(value) {
+  return /^\+?([1-9]\d*)$/.test(value);
+}
+function isTextValid(value) {
+  return /^[a-zA-Z]+$/.test(value);
+}
+
+
+
+
 
 
 
