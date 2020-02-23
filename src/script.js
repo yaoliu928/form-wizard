1b6a58dfdd7a2bb5be606861a9effc72e85a872d
bywdsg <liuyao928@tom.com>
2020-02-23 17:39:41 +1000

    add comments

--- src/script.js
+++ src/script.js
@@ -11,10 +11,15 @@ const resultItems = document.getElementsByClassName('result__item');
 const stTypeResult = document.querySelector('.result__item--select');
 
 initial();
+
+/*******************************************
+/     UI update
+/******************************************/
 function initial() {
   bindEvent();
   showForm(currentForm);
 }
+
 function showForm(num) {
   if (forms[num]) {
     forms[num].style.display = 'block';
@@ -22,24 +27,7 @@ function showForm(num) {
   showButton(num);
   showProgressBar();
 }
-function nextPrev(num) {
-  if (num === 1 && !isFormValid()) {
-    return false;
-  }
-  forms[currentForm].style.display = 'none';
-  currentForm += num;
-  if (currentForm === forms.length) {
-    submitForm();
-  }
-  showForm(currentForm);
-}
-function isFormValid() {
-  const valid = true;
-  const shownForm = forms[currentForm];
-  const isInputValid = checkInputFormat(shownForm, valid);
-  const isValid = checkSelectFormat(shownForm, isInputValid);
-  return isValid;
-}
+
 function showButton(num) {
   if (!num && prevBtn) {
     prevBtn.style.display = 'none';
@@ -56,6 +44,7 @@ function showButton(num) {
     }
   }
 }
+
 function showProgressBar() {
   if (!currentForm && completedBar) {
     completedBar.style.width = '5%';
@@ -68,6 +57,29 @@ function showProgressBar() {
     }
   }
 }
+
+function showResult() {
+  form.style.display = 'none';
+  result.style.display = 'block';
+  completedBar.style.width = '100%';
+  completedBar.innerHTML = '100%';
+}
+
+/*******************************************
+/     Form process
+/******************************************/
+function nextPrev(num) {
+  if (num === 1 && !isFormValid()) {
+    return false;
+  }
+  forms[currentForm].style.display = 'none';
+  currentForm += num;
+  if (currentForm === forms.length) {
+    submitForm();
+  }
+  showForm(currentForm);
+}
+
 function submitForm() {
   for (let i = 0; i < inputValue.length; i++) {
     const info = inputValue[i].value;
@@ -78,24 +90,7 @@ function submitForm() {
   showResult();
   return false;
 }
-function showResult() {
-  form.style.display = 'none';
-  result.style.display = 'block';
-  completedBar.style.width = '100%';
-  completedBar.innerHTML = '100%';
-}
-function bindEvent() {
-  if (prevBtn) {
-    prevBtn.addEventListener('click', () => nextPrev(-1));
-  };
-  if (nextBtn) {
-    nextBtn.addEventListener('click', () => nextPrev(1));
-  }
-}
-function unBindEvent() {
-  prevBtn.removeEventListener('click', () => nextPrev(-1));
-  nextBtn.removeEventListener('click', () => nextPrev(1));
-}
+
 function checkInputFormat(shownForm, valid) {
   const shownInputs = shownForm.getElementsByTagName('input');
   const shownNotice = shownForm.getElementsByClassName('notice');
@@ -111,6 +106,7 @@ function checkInputFormat(shownForm, valid) {
   }
   return valid;
 }
+
 function checkSelectFormat(shownForm, valid) {
   if (currentForm) {
     const shownSelect = document.getElementById('street-type');
@@ -124,6 +120,18 @@ function checkSelectFormat(shownForm, valid) {
   }
   return valid;
 }
+
+/*******************************************
+/     Validation
+/******************************************/
+function isFormValid() {
+  const valid = true;
+  const shownForm = forms[currentForm];
+  const isInputValid = checkInputFormat(shownForm, valid);
+  const isValid = checkSelectFormat(shownForm, isInputValid);
+  return isValid;
+}
+
 function isFormatValid(input) {
   switch (input.name) {
     case 'email':
@@ -143,25 +151,47 @@ function isFormatValid(input) {
       return true;
   }
 }
+
 function isEmailValid(value) {
   const emailFormat = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
   return emailFormat.test(value);
 }
+
 function isPhoneValid(value) {
   const phoneFormat = /^0[23478]\d{2}\s\d{3}\s\d{3}$/;
   return phoneFormat.test(value);
 }
+
 function isPostcodeValid(value) {
   return /^[0-9]{3,4}$/.test(value) && value >= 800 && value <= 7999;
 }
+
 function isStreetNumValid(value) {
   return /^[1-9]+[0-9]*$/.test(value);
 }
+
 function isTextValid(value) {
   return /^([a-zA-Z]+\s?)+$/.test(value);
 }
+
+/*******************************************
+/     bindings
+/******************************************/
+function bindEvent() {
+  if (prevBtn) {
+    prevBtn.addEventListener('click', () => nextPrev(-1));
+  };
+  if (nextBtn) {
+    nextBtn.addEventListener('click', () => nextPrev(1));
+  }
+}
+
+function unBindEvent() {
+  prevBtn.removeEventListener('click', () => nextPrev(-1));
+  nextBtn.removeEventListener('click', () => nextPrev(1));
+}
+
 module.exports = {
-  isFormatValid,
   isEmailValid,
   isPhoneValid,
   isPostcodeValid,
